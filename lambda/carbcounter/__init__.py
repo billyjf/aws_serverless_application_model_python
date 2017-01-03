3d3f9da7e060f2d06ea0cb4e0b55bc016937401a
Billy Fisher <billyjfisher@gmail.com>
2017-01-03 01:55:08 -0800

    ./deploy_carbcounter.sh, with addendums, yielded:
    http://billyjf.carbcounter.public.s3-website-us-west-2.amazonaws.com/

--- lambda/carbcounter/__init__.py
+++ lambda/carbcounter/__init__.py
@@ -1,19 +1,4 @@
-'''
-Follow these steps to complete the configuration of your command API endpoint
-
-  1. When completing the blueprint configuration select "Open" for security
-     on the "Configure triggers" page.
-
-  2. Enter a name for your execution role in the "Role name" field.
-     Your function's execution role needs kms:Decrypt permissions. We have
-     pre-selected the "KMS decryption permissions" policy template that will
-     automatically add these permissions.
-
-  3. Update the URL for your Slack slash command with the invocation URL for the
-     created API resource in the prod stage.
-'''
 from nutritionix import Nutritionix
-#import boto3
 import json
 import logging
 import os
@@ -30,20 +15,12 @@ def respond(err, res=None):
         'body': err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Expose-Headers': 'Webix-Token',
         },
     }
 
-
 def lambda_handler(event, context):
-    #POST: params = parse_qs(event['body'])
-    #token = params['token'][0]
-    #if token != expected_token:
-    #    logger.error("Request token (%s) does not match expected", token)
-    #    return respond(Exception('Invalid request token'))
-
-    #user = params['user_name'][0]
-    #command = params['command'][0]
-    #...
     search = ""
 
     if event != None and 'pathParameters' in event:
@@ -53,7 +30,6 @@ def lambda_handler(event, context):
         nix = Nutritionix(app_id="***REMOVED***",
                           api_key="***REMOVED***")
 
-        #nix_results = nix.search(search).json()
         nix_results = nix.search().nxql(
             fields=["item_id",
                     "item_name",
@@ -77,6 +53,6 @@ def lambda_handler(event, context):
                                                                   ['nf_total_carbohydrate'] )})
         count += 1
         pass
-
-    return respond(None, results)
+    
+    return respond(None, results['hits'])
 
