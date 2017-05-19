b83767fa2b921550fcfea383c6af37a2d5949a90
Billy Fisher <billyjfisher@gmail.com>
2017-05-18 22:12:51 -0700

    Clarify that first iteration may have manual steps, try not to forget them.

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ zip_push_to_s3_and_generate_deployment_cloudformation
 aws cloudformation deploy --template cloudformation/lambda_deploy.yaml --stack-name lambda-carbcounter --capabilities CAPABILITY_IAM
 integration_test
 
-echo "******** DON'T FORGET MANUAL STEPS:"
+echo "******** DON'T FORGET MANUAL STEPS (on first deployment/iteration, not necessary for all future deployments):"
 echo " 1. aws lambda add-permission --function-name [arn] --source-arn 'arn:aws:execute-api:us-west-2:649309545751:cruu7iixa4/*/*/carbcounter/*' --principal apigateway.amazonaws.com --statement-id efcea982-331a-4b0e-80d9-3edf5a5d3964 --action lambda:InvokeFunction"
 echo " 2? Actions > Enable CORS > accept defaults given to you via cloudformation prior deploy (strange)."
 echo " 4. Update client index.html and deploy static for new api gateway key --> factor this out in the future."
